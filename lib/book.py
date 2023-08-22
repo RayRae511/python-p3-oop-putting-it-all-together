61b021c593c7df40e02af8186f9da5b36a573945
Raymond Oeta <oetaray@gmail.com>
2023-08-22 16:00:55 +0300

    completed work

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page):
+        self.title = title
+        self.page = page
         
+    def title(self):
+        return self._title
+    
+    def title(self, title):
+        self._title = title
+        
+    def page(self):
+        return self._page
+    
+    def page(self, page):
+        if (page, int):
+            self._page = page
+        else:
+            print("page must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow you read fast! ")
+
+book = Book("And Then There Were None", 272)
+print(book.title)
+print(book.page)
+print(book.turn_page)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+      self.brand = brand
+      self.size = size
+        
+    def brand(self):
+        return self._brand
+    
+    def brand(self, brand):
+        self._brand = brand
+        
+    def size(self):
+        return self._size
+    
+    def shoe(self, size):
+        if (size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    def condition(self):
+        return self._condition
+    
+    def cobble(self):
+        self._condition = "New"
+        print("Your shoe is brand new!")
+
+stan_smith = Shoe("Adidas", 9)
+print(stan_smith.brand)
+print(stan_smith.size)
+print(stan_smith.cobble)
+print(stan_smith.condition)
